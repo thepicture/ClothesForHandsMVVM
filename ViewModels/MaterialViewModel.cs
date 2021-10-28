68363e8dff7b078fb2fa21afd4a4c33f74533081
thepicture <fuzzyeastern@protonmail.com>
2021-10-28 17:38:38 +0100

    Removed MaterialService and ICRUDOperationHandler

---
+++ ViewModels/MaterialViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ClothesForHandsMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClothesForHandsMVVM.ViewModels
+{
+    public class MaterialViewModel : ViewModelBase
+    {
+        private IEnumerable<Material> _materialsList;
+
+        public MaterialViewModel()
+        {
+            using (ClothesForHandsBaseEntities context = new ClothesForHandsBaseEntities())
+            {
+                MaterialsList = context.Materials.ToList();
+            }
+        }
+
+        public IEnumerable<Material> MaterialsList
+        {
+            get => _materialsList; set
+            {
+                _materialsList = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
