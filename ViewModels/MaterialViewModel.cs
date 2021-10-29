f97e9eeb76321ca2792b3e567ec70f024f1f3c67
thepicture <fuzzyeastern@protonmail.com>
2021-10-29 16:26:44 +0100

    Add checking for design mode and the designer's debug materials view

--- ViewModels/MaterialViewModel.cs
+++ ViewModels/MaterialViewModel.cs
@@ -11,6 +11,54 @@ namespace ClothesForHandsMVVM.ViewModels
         public MaterialViewModel()
         {
             _materialsList = new List<Material>();
+            #region DEBUG
+            if (System.ComponentModel.DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                _materialsList = new List<Material>
+                {
+                     new Material
+                    {
+                        Title = "Test Material 1",
+                        Cost = 1000,
+                        CountInPack = 5,
+                        CountInStock = 200,
+                        Image = @"\materials\material_8.jpeg",
+                        MinCount = 20,
+                        MaterialType = new MaterialType
+                        {
+                            Title = "Material Type 1",
+                        },
+                    },
+                      new Material
+                    {
+                        Title = "Test Material 2",
+                        Cost = 1000,
+                        CountInPack = 5,
+                        CountInStock = 200,
+                        Image = @"\materials\material_8.jpeg",
+                        MinCount = 20,
+                        MaterialType = new MaterialType
+                        {
+                            Title = "Material Type 2",
+                        },
+                    },
+                       new Material
+                    {
+                        Title = "Test Material 3",
+                        Cost = 1000,
+                        CountInPack = 5,
+                        CountInStock = 200,
+                        Image = null,
+                        MinCount = 20,
+                        MaterialType = new MaterialType
+                        {
+                            Title = "Material Type 3",
+                        },
+                    },
+                };
+                return;
+            }
+            #endregion
             new ClothesForHandsBaseEntities().Materials.ToList().ForEach(_materialsList.Add);
         }
 
