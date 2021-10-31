1b0d04f196025ba38af0c4990dd138e35def38af
thepicture <fuzzyeastern@protonmail.com>
2021-10-31 08:10:21 +0000

    Add MaterialBackgroundConverter and highlighting materials in MaterialView

---
+++ Converters/MaterialBackgroundConverter.cs
@@ -0,0 +1,34 @@
+﻿using ClothesForHandsMVVM.Models;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ClothesForHandsMVVM.Converters
+{
+    class MaterialBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Material material = value as Material;
+            double minCount = material.MinCount;
+            double? countInStock = material.CountInStock;
+            if (material.CountInStock < minCount)
+            {
+                return "#f19292";
+            }
+            else if (countInStock > 3 * minCount)
+            {
+                return "#ffba01";
+            }
+            else
+            {
+                return "#ffffff";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
