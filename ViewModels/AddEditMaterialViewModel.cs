ef8de61eda4c1a3c0fc6f5477ee1530dbfe6fcee
thepicture <fuzzyeastern@protonmail.com>
2021-11-01 13:35:53 +0000

    Add bindings to AddEditMaterialView and max length of types

--- ViewModels/AddEditMaterialViewModel.cs
+++ ViewModels/AddEditMaterialViewModel.cs
@@ -1,10 +1,30 @@
-﻿namespace ClothesForHandsMVVM.ViewModels
+﻿using ClothesForHandsMVVM.Models;
+
+namespace ClothesForHandsMVVM.ViewModels
 {
     public class AddEditMaterialViewModel : ViewModelBase
     {
+        private Material _material;
+
         public AddEditMaterialViewModel()
         {
+        }
 
+        public AddEditMaterialViewModel(Material material)
+        {
+            if (material != null)
+            {
+                Material = material;
+            }
+        }
+
+        public Material Material
+        {
+            get => _material; set
+            {
+                _material = value;
+                OnPropertyChanged();
+            }
         }
     }
 }
