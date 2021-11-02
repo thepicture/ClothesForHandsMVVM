6f9065824ef78fb3aceb8dd3119f2e9242c027d2
thepicture <fuzzyeastern@protonmail.com>
2021-11-02 07:49:20 +0000

    Added filtration edit types converter to remove all types item from it

---
+++ Converters/FiltrationEditTypesConverter.cs
@@ -0,0 +1,22 @@
+﻿using ClothesForHandsMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace ClothesForHandsMVVM.Converters
+{
+    public class FiltrationEditTypesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((List<MaterialType>)value).Where(m => !m.Title.Equals("Все типы"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
