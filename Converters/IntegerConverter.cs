4e2dbae2505c87a4d5d7e3bbee002c9d0768fc4d
thepicture <fuzzyeastern@protonmail.com>
2021-11-03 07:52:33 +0000

    Add textboxes key validation and fixed the binding errors of it

---
+++ Converters/IntegerConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ClothesForHandsMVVM.Converters
+{
+    public class IntegerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null
+                ? 0
+                : System.Convert.ToInt32(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return decimal.TryParse((string)value, out _)
+                ? (object)int.Parse(((string)value), CultureInfo.InvariantCulture)
+                : 0;
+        }
+    }
+}
