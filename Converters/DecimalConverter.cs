e31c47b934013aae6cdc566335047e0c9068bc6a
thepicture <fuzzyeastern@protonmail.com>
2021-11-03 07:46:09 +0000

    Implemented DecimalConverter

---
+++ Converters/DecimalConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ClothesForHandsMVVM.Converters
+{
+    public class DecimalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null
+                ? 0
+                : (decimal)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return decimal.TryParse((string)value, out _)
+                ? (object)decimal.Parse(((string)value).Replace(',', '.'), CultureInfo.InvariantCulture)
+                : .0;
+        }
+    }
+}
